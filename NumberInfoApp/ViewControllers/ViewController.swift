d92f32df36984738e7dbe97260dc4219b9518b8f
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-15 00:47:29 +0300

    Была создана ссылка.

--- NumberInfoApp/ViewControllers/ViewController.swift
+++ NumberInfoApp/ViewControllers/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private let link = "https://apilayer.net/api/validate?access_key=ed268fd18b92c030907e2edccb9b8764&number=+7(499)956-99-99"
     
     override func viewDidLoad() {
         super.viewDidLoad()
