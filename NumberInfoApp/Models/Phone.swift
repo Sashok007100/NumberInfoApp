d5567d09370fef2680db2347e9841dec27225ab0
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-15 00:18:58 +0300

    Была создана модель информации о номере телефона.

---
+++ NumberInfoApp/Models/Phone.swift
@@ -0,0 +1,19 @@
+//
+//  Phone.swift
+//  NumberInfoApp
+//
+//  Created by Alexandr Artemov (Mac Mini) on 15.07.2025.
+//
+
+struct Phone {
+    let valid: Bool
+    let number: String
+    let local_format: String
+    let international_format: String
+    let country_prefix: String
+    let country_code: String
+    let country_name: String
+    let location: String
+    let carrier: String
+    let line_type: String
+}
