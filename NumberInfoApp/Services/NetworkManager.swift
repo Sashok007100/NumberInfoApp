23ca8c0e47bf5192f95c87a105c4d3548440289f
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-07-22 01:14:19 +0300

    Создан класс NetworkManager и реализована логика получения данных с API.

--- NumberInfoApp/Services/NetworkManager.swift
+++ NumberInfoApp/Services/NetworkManager.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum NetworkError: Error {
+    case noData
+    case decodingError
+}
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetch(from url: URL, completion: @escaping(Result<Phone, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error ?? "No error description")
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                let phoneInfo = try decoder.decode(Phone.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(phoneInfo))
+                }
+            } catch {
+                completion(.failure(.decodingError))
+            }
+        }.resume()
+    }
+}
